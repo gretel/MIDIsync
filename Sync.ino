05b29ec3879921f0be2a7013c6360e1dd42970f2
Tom Hensel <tom@interpol8.net>
2012-08-14 02:15:16 +0300

    lot of changes
    implemented simpel state machine; added midi in commands; added software serial debug output; lots work todo!

--- Sync.ino
+++ Sync.ino
@@ -1,46 +1,51 @@
+#include <Button.h>
+#include <SoftwareSerial.h>
+// http://code.google.com/p/digitalwritefast/
+#include <digitalWriteFast.h>
+
 /*
 --------------------------------------------------------------------------------------------------
  MIDI Master Clock Generator
- Tom Hensel <tom@interpol8.net>
+ CC BY-SA 3.0 - 2012 - Tom Hensel <tom@interpol8.net>
  inspired by Gijs Gieskes 2009 http://gieskes.nl
  --------------------------------------------------------------------------------------------------
  */
 
-// http://code.google.com/p/digitalwritefast/
-#include <digitalWriteFast.h>
-
 // pins
 #define POT 0
-#define BUTTON_TOGGLE  2
-#define LED_BOARD  13
+#define BUTTON_TOGGLE 2
+#define LED_BOARD 13
 #define LED_CLOCK 6
+#define SERIAL_RX 11
+#define SERIAL_TX 12
 
 // static
-static const int STATUS_SYNC     = 0xF8;
-static const int STATUS_START    = 0xFA;
-static const int STATUS_CONTINUE = 0xFB;
-static const int STATUS_STOP     = 0xFC;
-static const int STATUS_RESET    = 0xFF;
+static const unsigned int STATUS_SYNC     = 0xF8;
+static const unsigned int STATUS_START    = 0xFA;
+static const unsigned int STATUS_CONTINUE = 0xFB;
+static const unsigned int STATUS_STOP     = 0xFC;
+static const unsigned int STATUS_RESET    = 0xFF;
+
+static const unsigned int BUTTON_PRESS    = 1;
+static const unsigned int BUTTON_HOLD     = 2;
 
 // var
 unsigned long time = 0;
+unsigned long currentMillis = 0;
+unsigned long previousMillis = 0;
 unsigned long analogValueA = 0;
+unsigned int counter = 0;
 unsigned int ledCounter = 0;
-boolean enabled = false;
-boolean prevEnabled = false;
+unsigned int currentState;
+unsigned int nextState;
+unsigned int buttonState;
 boolean ledFlag = LOW;
 boolean prevLedFlag = LOW;
 byte cmd;
 byte data1;
 byte data2;
-int buttonState;   // the previous reading from the input pin
-int lastButtonState = LOW;   // the previous reading from the input pin
-long lastDebounceTime = 0;  // the last time the output pin was toggled
-long debounceDelay = 50;    // the debounce time; increase if the output flickers
-
-//LED boardLED = LED(LED_BOARD);
-//LED clockLED = LED(LED_CLOCK);
-//Button button = Button(BUTTON_TOGGLE, HIGH);
+Button toggleButton = Button(BUTTON_TOGGLE, LOW);
+SoftwareSerial syncSerial(SERIAL_RX, SERIAL_TX); // RX, TX
 
 //http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1208715493/11
 #define FASTADC 1
@@ -54,7 +59,6 @@ long debounceDelay = 50;    // the debounce time; increase if the output flicker
 #endif
 
 void setup(){
-
 #if FASTADC
   // set prescale to 16
   sbi(ADCSRA,ADPS2);
@@ -62,82 +66,190 @@ void setup(){
   cbi(ADCSRA,ADPS0);
 #endif
 
+  // software serial
+  pinMode(SERIAL_RX, INPUT);
+  pinMode(SERIAL_TX, OUTPUT);
+  syncSerial.begin(19200);
+  syncSerial.println("SETUP");
+
+  // pin configuration
   pinMode(LED_BOARD, OUTPUT);
   pinMode(LED_CLOCK, OUTPUT);
   pinMode(BUTTON_TOGGLE, INPUT);
 
+  // button library
+  toggleButton.setDebounceDelay(50);
+  toggleButton.setHoldDelay(500);
+
   // decoration
   digitalWriteFast(LED_CLOCK, HIGH);
-  delay(250);
+  delay(100);
+  digitalWriteFast(LED_CLOCK, LOW);
+  delay(50);
+  digitalWriteFast(LED_CLOCK, HIGH);
+  delay(100);
   digitalWriteFast(LED_CLOCK, LOW);
 
   // midi rate
   Serial.begin(31250);
+
+  // initialize
+  currentState = nextState = STATUS_STOP;
+
+  // debug
+  syncSerial.println("READY");
 }
 
-void loop(){
+void loop()
+{
+  // check
   if(Serial.available() > 0)
   {
-    // TODO non-blocking buffer
+    // read a byte
     cmd = Serial.read();
-    data1 = Serial.read();
-    data2 = Serial.read();
 
-    Serial.write(cmd);
-    Serial.write(data1);
-    Serial.write(data2);
-  }
+    // debug
+    syncSerial.print("IN:");
+    syncSerial.print(cmd);
 
-  int reading = digitalRead(BUTTON_TOGGLE);
-
-  if (reading != lastButtonState) {
-    lastDebounceTime = millis();
-  } 
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    buttonState = reading;
-  }
-  // copy
-  lastButtonState = reading;
-
-  if(buttonState)
-  {
-    enabled = !enabled;
-  }
-
-  // compare to previous state
-  if(prevEnabled != enabled){
-    // copy
-    prevEnabled = enabled;
-
-    if(enabled)
+    // check for commands we will obey
+    if (cmd == STATUS_STOP || cmd == STATUS_START || cmd == STATUS_CONTINUE)
     {
-      // send start command
-      Serial.write(STATUS_START);
-      // ensure darkness
-      digitalWriteFast(LED_CLOCK, HIGH);
+      // change state according to command
+      setNextState(cmd);
+    }
+    else if(cmd == STATUS_SYNC)
+    {
+      // disable clock if we receive sync data (either our own, or from a daw or so)
+      // TODO TEST
+      setNextState(STATUS_STOP);
     } 
-    else{
+    else {
+      data1 = Serial.read();
+      data2 = Serial.read();
+
+      Serial.write(cmd);
+      Serial.write(data1);
+      Serial.write(data2);
+
+      // debug
+      syncSerial.print(":");
+      syncSerial.print(data1);
+      syncSerial.print(":");
+      syncSerial.print(data2);
+    }
+
+    // debug
+    syncSerial.println();
+  }
+
+  // button library
+  toggleButton.listen();
+
+  // check for button press (down)
+  if (toggleButton.onPress()) {
+    buttonState = BUTTON_PRESS;
+  }
+
+  // check if the button is being hold
+  if (toggleButton.isHold()) {
+    buttonState = BUTTON_HOLD;
+  }
+
+  // check for button release (up)
+  if (toggleButton.onRelease()) {
+    // ignore unbound states
+    if(!buttonState) {
+      return;
+    }
+
+    // debug
+    syncSerial.print("RELEASE:");
+    syncSerial.println(buttonState);
+
+    // check if clock is disabled
+    if(currentState == STATUS_STOP)
+    {
+      // single press, send continue command
+      if(buttonState == BUTTON_PRESS)
+      {
+        setNextState(STATUS_CONTINUE);
+      }
+      // button has been hold down, send (re)start command
+      else if(buttonState == BUTTON_HOLD)
+      {
+        setNextState(STATUS_START);
+      }
+    }
+    // check if clock is enabled
+    else if(currentState == STATUS_START || currentState == STATUS_CONTINUE)
+    {
+      // yep, stop
+      setNextState(STATUS_STOP);
+    }
+    else // failsafe
+    {
+      setNextState(STATUS_STOP);
+    }   
+  }
+
+  // compare
+  if(currentState != nextState)
+  {
+    // store state (for comparison on next round)
+    currentState = nextState;
+
+    // debug
+    syncSerial.print("STATE_CHANGE:");
+    syncSerial.print(currentState);
+    syncSerial.print("->");
+    syncSerial.println(nextState);
+
+    // check if clock is going to start
+    if(nextState == STATUS_START || nextState == STATUS_CONTINUE)
+    {
+      // send start or continue command
+      Serial.write(nextState);
+      // ensure brightness
+      digitalWriteFast(LED_CLOCK, HIGH);
+    }
+    else if(nextState == STATUS_STOP) 
+    {
       // send stop command
       Serial.write(STATUS_STOP);
       // ensure darkness
       digitalWriteFast(LED_CLOCK, LOW);
+      // reset
+      counter = 0;
+      // reset flag
       ledFlag = LOW;
-      delay(50);
+    }
+    else // failsafe
+    {  
+      currentState = STATUS_STOP;
     }
   }
 
-  if(enabled)
+  // check if clock is enabled
+  if(currentState == STATUS_START || currentState == STATUS_CONTINUE)
   {
     // map(value, fromLow, fromHigh, toLow, toHigh)
-    analogValueA = map(analogRead(POT), 0, 1023, 10, 100000);  // delay settings
+    analogValueA = map(analogRead(POT), 0, 1023, 7500, 200000);  // delay settings
 
     if(micros() - time > analogValueA){
       // copy
       time = micros();
 
+      // increment beat counter
+      counter++;
+
       // midi clock
       Serial.write(STATUS_SYNC);
-
+/*
+      // debug
+       syncSerial.print("SYNC:");
+       syncSerial.println(analogValueA);
+*/
       // increment
       ledCounter++;
       // compare
@@ -150,13 +262,22 @@ void loop(){
 
       // compare to previous state
       if(prevLedFlag != ledFlag){
-        // copy
+        // compare
         prevLedFlag = ledFlag;
-        // set output
+        // toggle outpit pin
         digitalWriteFast(LED_CLOCK, ledFlag);
       }
     }
   }
+}
+
+void setNextState(unsigned int state)
+{
+  // copy
+  nextState = state;
+  // DEBUG
+  //syncSerial.print("SET_NEXT_STATE:");
+  //syncSerial.println(state);
 }
 
 // end
